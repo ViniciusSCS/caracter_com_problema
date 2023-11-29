5dde5c0cbf4fdd01cd1b8405daea335c848e398e
Vinícius Siqueira <vinicius.siqueira@unievangelica.edu.br>
2023-11-29 17:34:37 -0300

    Adicionando novos testes

--- new.py
+++ new.py
@@ -1,9 +1,16 @@
 import csv
 
-# Nome do arquivo CSV
+# Nome do arquivo CSV de entrada
 arquivo_csv = 'arquivo.csv'
 
+# Nome do arquivo TXT com as palavras corrigidas
+arquivo_txt_corrigido = 'palavras_corrigidas.txt'
+
+# Nome do arquivo CSV de saída
 arquivo_csv_corrigido = 'arquivo_corrigido.csv'
+
+# Dicionário para armazenar as correções
+correcoes = {}
 
 # Nome do arquivo TXT de saída
 arquivo_txt = 'palavras_com_problema.txt'
@@ -39,23 +46,33 @@ with open(arquivo_txt, 'w', encoding='utf-8') as txt_file:
 
 print(f'Palavras com problema foram salvas em {arquivo_txt}.')
 
-# Função para substituir os caracteres ☺☺ por caracteres corretos
-def corrigir_caracteres(palavra):
-    return palavra.replace('Aten☺☺o', 'Atenção').replace('Qu☺☺mica', 'Química').replace('Ci☺☺ncia', 'Ciência').replace('incr☺☺vel', 'incrível')
+# Leitura do arquivo TXT com as palavras corrigidas
+with open(arquivo_txt_corrigido, 'r', encoding='utf-8') as txt_file:
+    for linha in txt_file:
+        # Divide a linha e remove espaços em branco
+        partes = linha.strip().split(',')
+        correcoes[partes[0]] = partes[1]
 
-# Leitura do arquivo CSV
+# Lista para armazenar as linhas corrigidas
+linhas_corrigidas = []
+
+# Função para aplicar correções nas palavras
+def corrigir_palavra(palavra):
+    return correcoes.get(palavra, palavra)
+
+# Leitura do arquivo CSV de entrada
 with open(arquivo_csv, 'r', encoding='utf-8') as csv_file:
     leitor_csv = csv.DictReader(csv_file)
-    
+
     # Processa cada linha do CSV
     for linha in leitor_csv:
         linha_corrigida = {
             'id': linha['id'],
-            'texto': corrigir_caracteres(linha['texto'])
+            'texto': ' '.join([corrigir_palavra(palavra) for palavra in linha['texto'].split()])
         }
         linhas_corrigidas.append(linha_corrigida)
 
-# Escreve as linhas corrigidas no mesmo arquivo CSV
+# Escreve as linhas corrigidas no arquivo CSV de saída
 with open(arquivo_csv_corrigido, 'w', encoding='utf-8', newline='') as csv_file:
     campos = ['id', 'texto']
     escritor_csv = csv.DictWriter(csv_file, fieldnames=campos)
@@ -67,4 +84,4 @@ with open(arquivo_csv_corrigido, 'w', encoding='utf-8', newline='') as csv_file:
     for linha_corrigida in linhas_corrigidas:
         escritor_csv.writerow(linha_corrigida)
 
-print(f'Caracteres corrigidos no arquivo CSV: {arquivo_csv_corrigido}.')
+print(f'Arquivo CSV corrigido salvo em {arquivo_csv_corrigido}.')
